a476a30c6c4880e9cdabefc148117b74af496bdd
Dennis T Kaplan <gruessle@gmail.com>
2011-12-25 17:17:08 -0800

    Restructuring

--- func/getRand.php
+++
@@ -1,8 +0,0 @@
-<?php
-function getRand(){
-    $fd = fopen("/dev/urandom","r");
-    $data = fread($fd,16);
-    fclose($fd);
-    return md5($data);
-}
-?>
--- func/randString.php
+++
@@ -1,8 +0,0 @@
-<?php
-function randString($length=6,$char="acdefghjkmnopqrstuvwxyz0123456789") {
-    $string = "";
-    for($i = 1; $i <= $length; $i++)
-        $string .= $char[rand(0,strlen($char)-1)];
-    return $string;
-}
-?>
